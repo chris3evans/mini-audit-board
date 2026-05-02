ab32560a7ff24aa33739ab605da56906525d654a
chris3evans <chris3evans.ce@gmail.com>
2026-05-02 11:01:36 +0100

    [Feature] - styling for dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
-import "./App.css";
+import styles from "./App.module.scss";
 import { Dashboard } from "./features/dashboard/dashboard/dashboard";
 
 function App() {
-  return <Dashboard />;
+  return (
+    <div className={styles["app"]}>
+      <Dashboard />
+    </div>
+  );
 }
 
 export default App;
