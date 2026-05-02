d2ba3d7ecb4804f07c5edf0f5b748223b09fc50d
chris3evans <chris3evans.ce@gmail.com>
2026-05-02 10:28:56 +0100

    [Feature] - moved invoice vendor into own component for practice

--- src/features/dashboard/invoice-item/invoice-item.tsx
+++ src/features/dashboard/invoice-item/invoice-item.tsx
@@ -1,12 +1,13 @@
 import type { IInvoiceItemProp } from "../../../types/client/client.interfaces";
 import { InvoiceId } from "./invoice-id/invoice-id.test";
 import styles from "./invoice-item.module.scss";
+import { InvoiceVendor } from "./invoice-vendor/invoice-vendor";
 
 export const InvoiceItem = ({ invoice }: IInvoiceItemProp) => {
   return (
     <div className={styles["invoice-item"]}>
       <InvoiceId id={invoice.id} />
-      <div>{invoice.vendor.name}</div>
+      <InvoiceVendor vendor={invoice.vendor.name} />
 
       <div>{invoice.invoice_number}</div>
       <div>{invoice.amount}</div>
