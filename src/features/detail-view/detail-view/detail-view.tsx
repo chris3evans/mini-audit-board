c43fa8f3901a4495a2efadb9fe3f0d3e79908658
chris3evans <chris3evans.ce@gmail.com>
2026-05-05 20:54:37 +0100

    [Feature] - render vendor details

--- src/features/detail-view/detail-view/detail-view.tsx
+++ src/features/detail-view/detail-view/detail-view.tsx
@@ -1,5 +1,5 @@
 import { useAppSelector } from "../../../hooks/state.hooks";
-import { useInvoices } from "../../../hooks/tanstack-query.hooks";
+import { useInvoices, useVendors } from "../../../hooks/tanstack-query.hooks";
 import styles from "./detail-view.module.scss";
 
 export const DetailView = ({}) => {
@@ -14,15 +14,20 @@ export const DetailView = ({}) => {
   );
 
   const { data: invoices, isLoading: invoicesLoading } = useInvoices();
+  const { data: vendors, isLoading: vendorsLoading } = useVendors();
 
   const selectedInvoice = invoices?.find(
     (invoice) => invoice.id === selectedInvoiceId,
+  );
+  const selectedVendor = vendors?.find(
+    (vendor) => vendor.id === selectedVendorId,
   );
 
   return (
     <div className={styles["detail-view"]}>
       <div className={styles["details"]}>
         {invoicesLoading && <div>Loading Invoice Details</div>}
+        {vendorsLoading && <div>Loading Vendor Details</div>}
         {selectedInvoiceId && (
           <div className={styles["detail-section"]}>
             <h3>Invoice</h3>
@@ -74,11 +79,58 @@ export const DetailView = ({}) => {
           </div>
         )}
 
-        <div className={styles["detail-section"]}>
-          <h3>Vendor</h3>
-          <div className={styles["detail-grid"]}></div>
-          {selectedVendorId}
-        </div>
+        {selectedVendorId && (
+          <div className={styles["detail-section"]}>
+            <h3>Vendor</h3>
+            <div className={styles["detail-grid"]}>
+              <div className={styles["detail-grid-item"]}>
+                <span>
+                  <b>Vendor Id:</b>
+                </span>
+                <span>{selectedVendor?.id ?? "Could not load Vendor Id"}</span>
+              </div>
+              <div className={styles["detail-grid-item"]}>
+                <span>
+                  <b>Name:</b>
+                </span>
+                <span>{selectedVendor?.id ?? "Could not load Name"}</span>
+              </div>
+              <div className={styles["detail-grid-item"]}>
+                <span>
+                  <b>VAT Number:</b>
+                </span>
+                <span>
+                  {selectedVendor?.vat_number ?? "Could not load VAT Number"}
+                </span>
+              </div>
+              <div className={styles["detail-grid-item"]}>
+                <span>
+                  <b>Account Number:</b>
+                </span>
+                <span>
+                  {selectedVendor?.account_number ??
+                    "Could not load Account Number"}
+                </span>
+              </div>
+              <div className={styles["detail-grid-item"]}>
+                <span>
+                  <b>New Vendor:</b>
+                </span>
+                <span>
+                  {selectedVendor?.new_vendor !== undefined
+                    ? `${selectedVendor.new_vendor}`
+                    : "Could not load New Vendor Status"}
+                </span>
+              </div>
+              <div className={styles["detail-grid-item"]}>
+                <span>
+                  <b>Rating:</b>
+                </span>
+                <span>{selectedVendor?.rating ?? "Could not load Rating"}</span>
+              </div>
+            </div>
+          </div>
+        )}
 
         <div className={styles["detail-section"]}>
           <h3>Address</h3>
