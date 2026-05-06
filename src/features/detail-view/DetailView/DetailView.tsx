5b0095d43f8feeb22c3566237938285bed5f94be
chris3evans <chris3evans.ce@gmail.com>
2026-05-06 14:41:17 +0100

    [Feature] - moved invoice section into own component

--- src/features/detail-view/DetailView/DetailView.tsx
+++ src/features/detail-view/DetailView/DetailView.tsx
@@ -9,6 +9,7 @@ import styles from "./DetailView.module.scss";
 import { ContainerCard } from "../../../components/ContainerCard/ContainerCard";
 import { SectionCard } from "../../../components/SectionCard/SectionCard";
 import { Button } from "../../../components/Button/Button";
+import { InvoiceSection } from "../InvoiceSection/InvoiceSection";
 
 export const DetailView = ({}) => {
   const selectedInvoiceId = useAppSelector(
@@ -61,59 +62,11 @@ export const DetailView = ({}) => {
         {vendorsLoading && <div>Loading Vendor Details</div>}
         {addressesLoading && <div>Loading Address Details</div>}
         {selectedInvoiceId && (
-          <SectionCard sectionTitle="Invoice">
-            <div className={styles["detail-grid"]}>
-              <div className={styles["detail-grid-item"]}>
-                <span>
-                  <b>Invoice Id:</b>
-                </span>
-                <span>
-                  {selectedInvoice?.id ?? "Could not load Invoice Id"}
-                </span>
-              </div>
-              <div className={styles["detail-grid-item"]}>
-                <span>
-                  <b>Invoice Number:</b>
-                </span>
-                <span>
-                  {selectedInvoice?.invoice_number ??
-                    "Could not load Invoice Number"}
-                </span>
-              </div>
-              <div className={styles["detail-grid-item"]}>
-                <span>
-                  <b>Invoice Amount:</b>
-                </span>
-                <span>
-                  {selectedInvoice?.amount ?? "Could not load Amount"}
-                </span>
-              </div>
-
-              <div className={styles["detail-grid-item"]}>
-                <span>
-                  <b>Invoice Currency:</b>
-                </span>
-                <span>{selectedInvoice?.id ?? "Could not load Currency"}</span>
-              </div>
-              <div className={styles["detail-grid-item"]}>
-                <span>
-                  <b>Invoice Status:</b>
-                </span>
-                <span>
-                  {selectedInvoice?.status ?? "Could not load Status"}
-                </span>
-              </div>
-              <div className={styles["detail-grid-item"]}>
-                <Button
-                  type="button"
-                  onClick={handleShowVendorClick}
-                  buttonText={`${renderVendor ? "Hide" : "Show"} Vendor`}
-                  size="medium"
-                  variant="light"
-                />
-              </div>
-            </div>
-          </SectionCard>
+          <InvoiceSection
+            selectedInvoice={selectedInvoice}
+            renderVendor={renderVendor}
+            onClick={handleShowVendorClick}
+          />
         )}
 
         {selectedVendorId && renderVendor && (
