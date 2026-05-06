fd56c2b8b8d8589fb7a5fb900d41de9db37d3cee
chris3evans <chris3evans.ce@gmail.com>
2026-05-06 12:03:15 +0100

    [Feature] - implemented basic colour theme changing with useContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import { Outlet } from "react-router-dom";
 import styles from "./App.module.scss";
+import { useTheme } from "./context/ThemeContext";
 
 function App() {
+  const { theme, toggleTheme } = useTheme();
+
   return (
-    <div className={styles["app"]}>
+    <div className={`${styles["app"]} ${styles[theme]}`} onClick={toggleTheme}>
       <Outlet />
     </div>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import { store } from "./state/store.ts";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routing/router.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ThemeProvider } from "./context/ThemeContext.tsx";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -19,7 +20,9 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <Provider store={store}>
-        <RouterProvider router={router}></RouterProvider>
+        <ThemeProvider>
+          <RouterProvider router={router}></RouterProvider>
+        </ThemeProvider>
       </Provider>
     </QueryClientProvider>
   </StrictMode>,
